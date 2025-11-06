ea8816150f0e72c017d8de347bc2a9275c0361cd
Andrey Shemet <andrey.shemet.nik@gmail.com>
2025-11-06 01:48:01 +0100

    fix task_1 and task_2

--- task_1.py
+++ task_1.py
@@ -14,9 +14,10 @@ def caching_fibonacci():
     return fibonacci
 
 
-# Отримуємо функцію fibonacci
-fib = caching_fibonacci()
+if __name__ == "__main__":
+    # Отримуємо функцію fibonacci
+    fib = caching_fibonacci()
 
-# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
-print(fib(10))  # Виведе 55
-print(fib(15))  # Виведе 610
+    # Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+    print(fib(10))  # Виведе 55
+    print(fib(15))  # Виведе 610
